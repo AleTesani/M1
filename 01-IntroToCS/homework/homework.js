f1aaa6bf7aa150fa5942917ff6f04be0947a6fe3
AleTesani <99367779+AleTesani@users.noreply.github.com>
2022-05-23 20:45:39 -0300

    Add files via upload

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  let total = 0;
+  for(let i =0; i < num.length; i++){
+    total = total + num[i] * Math.pow(2, num.length -1 -i); 
+  }
+  return total;
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  let adividir = num
+  let binario = []
+ while (adividir != 0) {
+   binario.push(adividir % 2)
+   adividir = Math.floor(adividir/2)
+ }
+return binario.reverse().join("")
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
